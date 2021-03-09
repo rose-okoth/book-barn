a3d6d8b9800b0c03de3aa73444270978666f062f
Stephen Ndele <stephenndele09346@gmail.com>
2021-03-09 14:40:54 +0300

    add start sh

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
-export books_api_key=roseokoth
-export secret_key=123456
+export books_api_key='roseokoth'
+export SECRET_KEY='123456'
+export DATABASE_URL='postgresql+psycopg2://moringa:Access@localhost:5432/books'
+export MAIL_USERNAME='oneminutepitchhome@gmail.com'
+export MAIL_PASSWORD='Newbegginig2021!'
 
-python3 manage.py server
+
+python3 manage.py runserver
