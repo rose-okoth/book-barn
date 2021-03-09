56763376183f132b5b1f932044238a95eb101cc6
ANTONYMATHENGE <tonnykogi@gmail.com>
2021-03-09 20:23:44 +0300

    intalled requests and imported it

--- app/request.py
+++ app/request.py
@@ -6,3 +6,19 @@ def configure_request(app):
     # api_key = app.config['books_api_key']
     # base_url = app.config["book_api_base_url"]
 
+#
+import requests
+from flask import Flask
+from flask import render_template
+api_key = '67786b6df7f94b37b826f0360c99094c'
+url = f'https://newsapi.org/v2/sources?apiKey={api_key}'
+response = requests.get(url) 
+app = Flask(__name__)
+@app.route('/')
+def home():
+    sources = []
+    for data in response.json()['sources']:
+        sources.append(data)
+    return render_template('test.html', sources=sources)
+if __name__ == '__main__':
+    app.run(debug=True)
