0bc16a8c8c2524829b0090b2cde01e34cd237ab4
Stephen Ndele <stephenndele09346@gmail.com>
2021-03-09 14:40:18 +0300

    add main config file

--- config.py
+++ config.py
@@ -4,7 +4,16 @@ class Config:
     
     book_api_base_url =''
     books_api_key = os.environ.get('books_api_key')
-    secret_key = os.environ.get('secret_key')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+
+
 
 class ProdConfig(Config):
     
@@ -12,6 +21,8 @@ class ProdConfig(Config):
 
 
 class DevConfig(Config):
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost:5432/books'
     
     DEBUG = True
 
