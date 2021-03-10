5231630247465da528c57c91ee7af34fe97b9a89
rose-okoth <okoth.rose0@gmail.com>
2021-03-10 13:19:41 +0300

    changed init file

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,6 @@ from werkzeug.datastructures import  FileStorage
 db = SQLAlchemy()
 mail = Mail()
 bootstrap = Bootstrap()
-ft-authentication
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
@@ -30,11 +29,6 @@ def create_app(config_name):
     app.register_blueprint(authentication_blueprint)
     app.register_blueprint(main_blueprint)
 
-
-    # Initializing flask extensions
-    bootstrap.init_app(app)
-    login_manager.init_app(app)
-    db.init_app(app)
     # configure_uploads(app,photos)
     mail.init_app(app)
     app.secret_key = "123456"
@@ -43,6 +37,8 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     mail.init_app(app)
+    login_manager.init_app(app)
+
 
     # setting config
     from .request import configure_request
