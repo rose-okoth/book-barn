dede03af0000c0e1e7cc1d6a837bb135e5caf4b5
nicholas-oduor <oduor5742@gmail.com>
2021-03-10 08:18:40 +0300

    added email configurations in config.py app

--- config.py
+++ config.py
@@ -1,10 +1,23 @@
 import os 
 
 class Config:
-    
+    QUOTE_API_BASE_URL ='http://quotes.stormconsultancy.co.uk/random.json'
+
     book_api_base_url =''
     books_api_key = os.environ.get('books_api_key')
     secret_key = os.environ.get('secret_key')
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
 
 class ProdConfig(Config):
     
