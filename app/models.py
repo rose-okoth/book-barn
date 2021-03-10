5ec1d1481d80e807940e3ed7cd770bef50b5c9fb
nicholas-oduor <oduor5742@gmail.com>
2021-03-10 08:26:29 +0300

    modified models & request files in templates folder

--- app/models.py
+++ app/models.py
@@ -1,6 +1,132 @@
-class Book:
-    '''
-    Book class to define Book Objects
-    '''
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+from datetime import datetime
 
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+  
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pass_secure = db.Column(db.String(255))
+    opinion = db.relationship('Opinion', backref='username', lazy='dynamic')
+    comments = db.relationship('Comment', backref='username', lazy=True)
+    
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+    
+    def __repr__(self):
+        return f'User {self.username}'
+    
+    class Writer(UserMixin,db.Model):
+    __tablename__ = 'writers'
+
+    id = db.Column(db.Integer,primary_key = True)
+    writer_name = db.Column(db.String(255),index = True)
+    writer_email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    writer_pass_secure = db.Column(db.String(255))
+    
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.writer_pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.writer_pass_secure,password)
+    
+    def __repr__(self):
+        return f'User {self.username}'
+    
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+    writers = db.relationship('Writer',backref = 'role',lazy="dynamic")
+    
+
+    def __repr__(self):
+        return f'User {self.name}'
+    
+
+class Opinion(db.Model):
+    __tablename__ = 'opinions'
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    opinion_title = db.Column(db.String(255), index=True)
+    description = db.Column(db.String(255), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    
+
+    def save_opinion(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_opinions(cls, id):
+        opinions = Post.query.filter_by(id=id).all()
+        return opinions
+
+    @classmethod
+    def get_all_opinions(cls):
+        opinions = Opinion.query.order_by('-id').all()
+        return opinions
+
+    def __repr__(self):
+        return f'Posts {self.opinion_title}'
+    
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.Text())
+    opinion_id = db.Column(db.Integer, db.ForeignKey('opinions.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(cls, opinion_id):
+        comments = Comment.query.filter_by(opinion_id=opinion_id).all()
+        return comments
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'Comments: {self.comment}'
     
--- app/request.py
+++ app/request.py
@@ -1,8 +1,24 @@
-import urllib.request,json
-from .models import Book
+# import urllib.request,json
+# from .models import Book
 
-def configure_request(app):
-    global api_key,base_url
+# def configure_request(app):
+#     global api_key,base_url
     # api_key = app.config['books_api_key']
     # base_url = app.config["book_api_base_url"]
 
+import urllib.request,json
+
+base_url = None
+
+def configure_request(app):
+    global base_url
+    base_url = app.config['QUOTE_API_BASE_URL']
+
+def get_quote():
+    get_quote_url ='http://quotes.stormconsultancy.co.uk/random.json'
+
+    with urllib.request.urlopen(get_quote_url) as url:
+        get_quote_data = url.read()
+        get_quote_response = json.loads(get_quote_data)
+
+    return get_quote_response
